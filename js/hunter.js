cefc5069519dab8e8e4938665a5b21da20e07f41
Mehedi Hasan <mhedih32@gmail.com>
2024-02-29 08:45:03 +0600

    search and show the value added

--- js/hunter.js
+++ js/hunter.js
@@ -1,33 +1,73 @@
-const loadPhon = async () => {
-  const res = await fetch(
-    "https://openapi.programming-hero.com/api/phones?search=iphone"
-  );
+const loadPhone = async (searchText) => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
   const data = await res.json();
   const phones = data.data;
-  //   console.log(phone);
-  displayPhon(phones);
-};
+  //   console.log(phones);
+  displayPhones(phones);
+}
 
-const displayPhon = (phones) => {
-    const phoneContainer=document.getElementById('card-container');
+const displayPhones = phones => {
+  console.log(phones);
 
-  phones.forEach((phone) => {
-    console.log(phone);
-    const phoneCard = document.createElement("div");
-    phoneCard.classList = `card card-compact w-96 bg-base-100 shadow-xl`;
+  const phoneContainer = document.getElementById('phone-container');
+  phoneContainer.textContent = '';
+  // show all container
+  const showAllContainer=document.getElementById('show-all-container');
+  if(phones.length>12){
+    showAllContainer.classList.remove('hidden');
+  }else{
+    showAllContainer.classList.add('hidden');
+  }
+  // 12ta. koyta phone dekhabo ta likha holo
+  phones=phones.splice(0,12)
+  phones.forEach(phone => {
+    // console.log(phone);
+    // step-2: create a div
+    const phoneCard = document.createElement('div');
+    phoneCard.classList = `card w-80 px-3 pb-2 pt-10 bg-base-100 shadow-xl`;
+    //step-3: set innerHTML
     phoneCard.innerHTML = `
     <figure><img src="${phone.image}" alt="Phone" /></figure>
     <div class="card-body">
       <h2 class="card-title">${phone.phone_name}</h2>
       <h5 class='text-xl'>Brand : ${phone.brand}</h5>
-      <p>If a dog chews shoes whose shoes does he choose?</p>
+      
       <div class="card-actions justify-end">
         <button class="btn btn-primary">Buy Now</button>
       </div>
     </div>
     `;
-    phoneContainer.appendChild(phoneCard)
+    //step-5: append child
+    phoneContainer.appendChild(phoneCard);
   });
+  toggleLoadingSpinner(false);
 };
 
-loadPhon();
+// handle search
+const handleSearch = () => {
+  toggleLoadingSpinner(true)
+  const searchField = document.getElementById('search-field');
+  const searchText = searchField.value;
+  console.log(searchText);
+  loadPhone(searchText);
+}
+// ------ 2nd input field--------
+const handleSearch2 = () =>{
+  toggleLoadingSpinner(true)
+  const searchField=document.getElementById('search-field2')
+  const searchTexts2=searchField.value;
+  loadPhone(searchTexts2);
+}
+const toggleLoadingSpinner=(isLoading)=>{
+    const loadingSpinner=document.getElementById('loading-spinner');
+    if(isLoading){
+      loadingSpinner.classList.remove('hidden')
+    }else{
+      loadingSpinner.classList.add('hidden')
+    }
+}
+
+// loadPhon();
+
+
+{/* <p>If a dog chews shoes whose shoes does he choose?</p> */}
